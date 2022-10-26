6c766fd2ff17f486e414bcf45d1e69ed0ec933e5
Eruin de Lima Silva <eruindelimasilva@MacBook-Air-de-Eruin.local>
2022-10-26 00:12:15 -0300

    primeiro commit

---
+++ Cadastros/app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use App\Models\User;
+use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class RegisterController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Register Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles the registration of new users as well as their
+    | validation and creation. By default this controller uses a trait to
+    | provide this functionality without requiring any additional code.
+    |
+    */
+
+    use RegistersUsers;
+
+    /**
+     * Where to redirect users after registration.
+     *
+     * @var string
+     */
+    protected $redirectTo = RouteServiceProvider::HOME;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+
+    /**
+     * Get a validator for an incoming registration request.
+     *
+     * @param  array  $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            // 'nome_empresa' => ['required', 'string', 'nome_empresa', 'max:255'],
+            // 'servicos' => ['required', 'string', 'servicos', 'max:255'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'telefone' => ['required', 'string', 'telefone', 'max:45'],
+        ]);
+    }
+
+    /**
+     * Create a new user instance after a valid registration.
+     *
+     * @param  array  $data
+     * @return \App\Models\User
+     */
+    protected function create(array $data)
+    {
+        return User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            // 'nome_empresa' => $data['nome_empresa'],
+            // 'servicos' => $data['servicos'],
+            'password' => Hash::make($data['password']),
+            'telefone' => $data['telefone'],
+        ]);
+    }
+}
---
+++ Cadastros/resources/views/auth/register.blade.php
@@ -0,0 +1,105 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Register') }}</div>
+
+                <div class="card-body">
+                    <form method="POST" action="{{ route('register') }}">
+                        @csrf
+
+                        <div class="row mb-3">
+                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Nome') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+                                @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                        
+
+                        <div class="row mb-3">
+                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <label for="telefone" class="col-md-4 col-form-label text-md-end">{{ __('Telefone') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="telefone" type="text" class="form-control @error('telefone') is-invalid @enderror" name="telefone" value="{{ old('telefone') }}" required autocomplete="name" placeholder="(99) 9.9999-9999" autofocus>
+                            </div>
+                        </div>
+                        <!-- <div class="row mb-3">
+                            <label for="empresa" class="col-md-4 col-form-label text-md-end">{{ __('Nome da empresa') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="nome_empresa" type="text" class="form-control @error('nome_empresa') is-invalid @enderror" name="nome_empresa" value="{{ old('nome_empresa') }}" required autocomplete="name"autofocus>
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <label for="servicos" class="col-md-4 col-form-label text-md-end">{{ __('Serviços') }}</label>
+                            <div class="col-md-6">
+                            <input class="form-check-input" type="checkbox" id="servicos" value="option1">
+                            <label class="form-check-label" for="inlineCheckbox1">&nbsp Social Media</label></br>
+                            <input class="form-check-input" type="checkbox" id="servicos" value="option1">
+                            <label class="form-check-label" for="inlineCheckbox1">&nbsp CRM</label></br>
+                            <input class="form-check-input" type="checkbox" id="servicos" value="option1">
+                            <label class="form-check-label" for="inlineCheckbox1">&nbsp Mídia</label></br>
+                            <input class="form-check-input" type="checkbox" id="servicos" value="option1">
+                            <label class="form-check-label" for="inlineCheckbox1">&nbsp SEO</label></br>
+                            </div>
+                        </div> -->
+
+                        <div class="row mb-3">
+                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Senha') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirmar Senha') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                            </div>
+                        </div>
+
+                        <div class="row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Registrar') }}
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
